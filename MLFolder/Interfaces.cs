9261f45ea8c17b4659d946817cabd9962ab1699a
SubAtomic <61936215+2a34-cmd@users.noreply.github.com>
2022-06-26 12:54:41 +0300

    Adding BP and converting to decimal
    thanks raminrahimzada
    chech it out  <a>here</a href = "github.com/raminrahimzada/CSharp-Helper-Classes/tree/master/Math/DecimalMath">

---
+++ MLFolder/Interfaces.cs
@@ -0,0 +1,9 @@
+﻿namespace Atomic.ArtificialNeuralNetwork.libraries
+{
+    internal interface IWeightBias
+    {
+        //decimal? BackProp(decimal[] Expected);
+        //void SetWB(decimal[] Expected);
+        //void SetWB(decimal Diff);
+    }
+}
--- MLFolder/Program.cs
+++ MLFolder/Program.cs
@@ -1,62 +1,52 @@
-﻿using System.Collections.Generic;
-using System.Threading;
-using Atomic.ArtificialNeuralNetwork.libraries;
-namespace Atomic.NeuralNetworkLib.Driver
+﻿using Atomic.ArtificialNeuralNetwork.libraries;
+using System.Linq;
+namespace Atomic.ArtificialNeuralNetwork.Driver
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\Khtably55\Desktop\NeuralNetwork\TestFolder\config2.mn1";
+            string path = @"TestFolder\config4.mn1";
             ConfigFile config = new(path);
             config.CreateANN();
             NeuralNetwork network = NetworkDic.Networks[1];
             network.Finilize();
-            //network.FirstWork();
-            //ThreadsDic.Initialize(12);
-            //for (int i = 0; i < 12; i++)
-            //{
-            //    if (i < 4) ThreadsDic.threads["Thread" + i].Action = network.DividedWorkOfLayer;
-            //    else ThreadsDic.threads["Thread" + i].Action = network.DividedWorkOfNeuron;
-            //}
-            //List<ParallelProccess> threads = new();
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    threads.Add(ThreadsDic.threads["Thread" + i]);
-            //}
-            //List<List<ParallelProccess>> parallels = new List<List<ParallelProccess>>();
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    parallels.Add(new List<ParallelProccess> { ThreadsDic.threads[$"Thread{i + 4}"], ThreadsDic.threads[$"Thread{i + 8}"] });
-            //}
-
-            //for (int j = 9; j > 7; j--)
-            //{
-            //    List<List<Neuron>> ListofLists = NeuralNetwork.DivideList(network.layerList[j].neuronList, 4);
-            //    for (int i = 0; i < 4; i++)
-            //    {
-            //        threads[i].Start(new Int4
-            //        {
-            //            i = i,
-            //            j = 2,
-            //            k = 4,
-            //            l = j,
-            //            Threads = parallels[i]
-            //        });
-            //    }
-            //    foreach (var thr in threads) thr.Join();
-            //}
-            //foreach (var item in ThreadsDic.threads.Values)
-            //{
-            //    item.Kill();
-            //}
             network.InfowB();
             System.Console.WriteLine("------------------------------------------------------");
-            network.FindPaths(true);
-            System.Console.WriteLine($"Serial   found:{PathDic.Paths.Count} path --  time taken:{network.Serial}");
             PathDic.Paths.Clear();
-            network.FindPathsParallel(true);
-            System.Console.WriteLine($"Parallel   found:{PathDic.Paths.Count} path --  time taken:{network.Paralleltime}");
+            network.FindPaths();
+            PathDic.InfoOfPaths();
+            System.Console.WriteLine("------------------------------------------------------" + '\n' + "Before training");
+            decimal[][] inputs = Enumerable.Range(0, 10).Select(i => new decimal[] { i, i * 0.1m }).ToArray();
+            decimal[][] trainingSet = Enumerable.Range(0, 10).Select(i => new decimal[] { i * 0.05m }).ToArray();
+            var doubles1 = Enumerable.Range(0, 10).Select(i => network.SquereCost(trainingSet[i].ToList(), inputs[i])).ToArray();
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                network.Calculate(inputs[i]);
+                network.InfowC();
+                System.Console.WriteLine(doubles1[i]);
+            }
+            decimal[] doubles2 = new decimal[10];
+            for (int j = 0; j < 10; j++)
+            {
+                network.PatchProccess(trainingSet, inputs,0.001m);
+                System.Console.WriteLine("------------------------------------------------------" + '\n' + "After training");
+                network.InfowB();
+                doubles1 = doubles2;
+                doubles2 = Enumerable.Range(0, 10).Select(i => network.SquereCost(trainingSet[i].ToList(), inputs[i])).ToArray();
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    network.Calculate(inputs[i]);
+                    network.InfowC();
+                    System.Console.WriteLine(doubles2[i]);
+                }
+                network.InfowB();
+                for (int i = 0; i < doubles1.Length; i++)
+                {
+                    System.Console.WriteLine($"Total gain :{doubles1[i] - doubles2[i]}");
+                }
+            }
+            
         }
     }
 }
@@ -125,4 +115,44 @@ namespace Atomic.NeuralNetworkLib.Driver
 //string path = @"C:\Users\" + user + @"\Desktop\NeuraalNetwork\TestFolder\config.mn1";//ConfigFile config = new ConfigFile(path);
 //config.CreateANN();
 //NetworkDic.Networks[0].InfowB();
+#endregion
+#region //othher commands
+//network.FirstWork();
+//ThreadsDic.Initialize(12);
+//for (int i = 0; i < 12; i++)
+//{
+//    if (i < 4) ThreadsDic.threads["Thread" + i].Action = network.DividedWorkOfLayer;
+//    else ThreadsDic.threads["Thread" + i].Action = network.DividedWorkOfNeuron;
+//}
+//List<ParallelProccess> threads = new();
+//for (int i = 0; i < 4; i++)
+//{
+//    threads.Add(ThreadsDic.threads["Thread" + i]);
+//}
+//List<List<ParallelProccess>> parallels = new List<List<ParallelProccess>>();
+//for (int i = 0; i < 4; i++)
+//{
+//    parallels.Add(new List<ParallelProccess> { ThreadsDic.threads[$"Thread{i + 4}"], ThreadsDic.threads[$"Thread{i + 8}"] });
+//}
+
+//for (int j = 9; j > 7; j--)
+//{
+//    List<List<Neuron>> ListofLists = NeuralNetwork.DivideList(network.layerList[j].neuronList, 4);
+//    for (int i = 0; i < 4; i++)
+//    {
+//        threads[i].Start(new Int4
+//        {
+//            i = i,
+//            j = 2,
+//            k = 4,
+//            l = j,
+//            Threads = parallels[i]
+//        });
+//    }
+//    foreach (var thr in threads) thr.Join();
+//}
+//foreach (var item in ThreadsDic.threads.Values)
+//{
+//    item.Kill();
+//}
 #endregion
